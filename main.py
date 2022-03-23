63767b9c678d01b6411d39ff951530d6e9a87db4
ftorresc <32510211+ftorresc@users.noreply.github.com>
2022-03-23 21:23:32 +0100

    remove metadata json from previous scraps

--- main.py
+++ main.py
@@ -13,3 +13,5 @@ UNTIL = datetime(2022, 3, 1)
 for post in takewhile(lambda p: p.date > UNTIL, dropwhile(lambda p: p.date > SINCE, posts)):
     print(post.date)
     L.download_post(post, "instagram")
+
+# instaloader --login azuretaol546 --password death34server --no-pictures --no-videos --no-metadata-json danaherjohn
