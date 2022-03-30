8db86c2cd878d9e74a2055e033475aa8e74ac6c7
ftorresc <32510211+ftorresc@users.noreply.github.com>
2022-03-30 14:04:11 +0200

    add generic text to speech

--- randompick.py
+++ randompick.py
@@ -1,5 +1,6 @@
 import os
 import random
+import pyttsx3
 
 path = r"./danaherjohn/"
 #for file in os.listdir(path):
@@ -9,7 +10,12 @@ d = random.choice(files)
 
 with open(path + d, 'r') as file:
     for line in file:
+       # debugging purposes, allows to exclude non-complying files 
+       # print(d)
         print(line)
+        engine = pyttsx3.init()
+        engine.say(line)
+        engine.runAndWait()
 # print(random.choice(os.listdir(path)))
 
 
